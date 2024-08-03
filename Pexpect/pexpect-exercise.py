5e63e7662e6ecc8c608f613654fc0f7cd0466021
JosephDotGit <65070037@kmitl.ac.th>
2024-08-03 15:08:41 +0000

    Change sourc folder and add Telenetlib set VRF control-data and IP address of interface g0/1 and g0/2 or R1

--- Pexpect/pexpect-exercise.py
+++ Pexpect/pexpect-exercise.py
@@ -1,15 +1,17 @@
 import pexpect
 import sys
+import os
 #R1-13
 R1_ip = "172.31.113.3"
-R1_username = "admin"
-R1_password = "cisco"
+R1_username = os.environ.get('TELNET_USER')
+R1_password = os.environ.get('TELNET_PASSWORD')
 R1_command = 'ip address 172.16.1.1 255.255.255.255'
 
 try:
 
     child = pexpect.spawn('telnet %s'%R1_ip)
     child.expect('Username:')
+    print(type(R1_username))
     child.sendline(R1_username)
     child.expect('Password:')
     child.sendline(R1_password)
@@ -29,8 +31,8 @@ print("Loopback0 172.16.1.1 is created on %s"%R1_ip)
 
 #R2-13
 R2_ip = "172.31.113.4"
-R2_username = "admin"
-R2_password = "cisco"
+R2_username = os.environ.get('TELNET_USER')
+R2_password = os.environ.get('TELNET_PASSWORD')
 R2_command = 'ip address 172.16.2.2 255.255.255.255'
 try:
     child = pexpect.spawn('telnet %s'%R2_ip)
